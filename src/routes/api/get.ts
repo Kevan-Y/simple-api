7d2d5b36f73860940b6bc3041c051991be8d27f6
Kevan-Y <58233223+Kevan-Y@users.noreply.github.com>
2022-04-19 19:43:27 -0400

    Change to pong test

--- src/routes/api/get.ts
+++ src/routes/api/get.ts
@@ -2,5 +2,5 @@ import { Request, Response } from 'express';
 import { createSuccessResponse } from '../../utils/response';
 
 export const pong = (req: Request, res: Response) => {
-  res.status(200).json(createSuccessResponse({ message: 'pong1' }));
+  res.status(200).json(createSuccessResponse({ message: 'pong testing' }));
 };
